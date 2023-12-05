3b367339efd1d15b24d14bd8ca4a83d1436eda32
Ysabel Colina <yzame.marie@gmail.com>
2023-12-05 21:50:28 +0800

    goods

--- app/src/main/java/com/example/matutor/Login.java
+++ app/src/main/java/com/example/matutor/Login.java
@@ -56,29 +56,42 @@ public class Login extends AppCompatActivity {
 
                 if (!email.isEmpty() && Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
                     if (!password.isEmpty()) {
+                        // Check in the "user_learner" collection
                         firestore.collection("user_learner")
                                 .whereEqualTo("learnerEmail", email)
                                 .get()
                                 .addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
                                     @Override
-                                    public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
-                                        if (!queryDocumentSnapshots.isEmpty()) {
-                                            auth.signInWithEmailAndPassword(email, password)
-                                                    .addOnCompleteListener(Login.this, new OnCompleteListener<AuthResult>() {
-                                                        @Override
-                                                        public void onComplete(@NonNull Task<AuthResult> authTask) {
-                                                            if (authTask.isSuccessful()) {
-                                                                Toast.makeText(getApplicationContext(), "Login Successful!", Toast.LENGTH_SHORT).show();
-                                                                Intent intent = new Intent(getApplicationContext(), Dashboard.class);
-                                                                startActivity(intent);
-                                                                finish();
+                                    public void onSuccess(QuerySnapshot learnerSnapshots) {
+                                        if (!learnerSnapshots.isEmpty()) {
+                                            // User found in "user_learner" collection
+                                            loginUser(email, password);
+                                        } else {
+                                            // User not found in "user_learner" collection, check in "user_tutor" collection
+                                            firestore.collection("user_tutor")
+                                                    .whereEqualTo("tutorEmail", email)
+                                                    .get()
+                                                    .addOnCompleteListener(tutorTask -> {
+                                                        if (tutorTask.isSuccessful()) {
+                                                            QuerySnapshot tutorSnapshots = tutorTask.getResult();
+
+                                                            if (tutorSnapshots != null) {
+                                                                if (!tutorSnapshots.isEmpty()) {
+                                                                    // User found in "user_tutor" collection
+                                                                    loginUser(email, password);
+                                                                } else {
+                                                                    // User not found in "user_tutor" collection
+                                                                    Toast.makeText(getApplicationContext(), "User not found in user_tutor collection", Toast.LENGTH_SHORT).show();
+                                                                }
                                                             } else {
-                                                                Toast.makeText(getApplicationContext(), "Login Failed!", Toast.LENGTH_SHORT).show();
+                                                                // Log an error message if tutorSnapshots is null
+                                                                Toast.makeText(getApplicationContext(), "tutorSnapshots is empty", Toast.LENGTH_SHORT).show();
                                                             }
+                                                        } else {
+                                                            // Handle the failure case
+                                                            Toast.makeText(getApplicationContext(), "Error: " + tutorTask.getException().getMessage(), Toast.LENGTH_SHORT).show();
                                                         }
                                                     });
-                                        } else {
-                                            Toast.makeText(getApplicationContext(), "User not found!", Toast.LENGTH_SHORT).show();
                                         }
                                     }
                                 })
@@ -93,10 +106,9 @@ public class Login extends AppCompatActivity {
                 } else {
                     binding.emailInput.setError("Please enter a valid email address.");
                 }
-
             }
-
         });
+
 
         //register here button
         binding.regHereButton.setOnClickListener(new View.OnClickListener() {
@@ -132,4 +144,28 @@ public class Login extends AppCompatActivity {
         builder.show();
     }
 
+    private void loginUser(String email, String password) {
+        auth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(Login.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> authTask) {
+                        if (authTask.isSuccessful()) {
+                            Toast.makeText(getApplicationContext(), "Login Successful!", Toast.LENGTH_SHORT).show();
+                            Intent intent = new Intent(getApplicationContext(), Dashboard.class);
+                            startActivity(intent);
+                            finish();
+                        } else {
+                            // Check if the failure is due to user not being found
+                            if (authTask.getException() != null &&
+                                    authTask.getException().getMessage() != null &&
+                                    authTask.getException().getMessage().contains("no user record corresponding")) {
+                                // Do nothing or handle as needed
+                            } else {
+                                // Display the toast for other login failures
+                                Toast.makeText(getApplicationContext(), "Login Failed!", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    }
+                });
+    }
 }
